9d69262e432a19c2d0fe4cc9bc54a4a92785c785
Ryosuke Kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2020-09-25 17:12:54 +0000

    Update: for experiments

--- sample_linear01/run_linear.sh
+++ sample_linear01/run_linear.sh
@@ -1,12 +1,12 @@
 
-dios-linear train --config config.json --method MOESP
+#dios-linear train --config config.json --method MOESP
 dios-linear test  --config config.json --method MOESP
-dios-linear train --config config.json --method MOESP_auto
+#dios-linear train --config config.json --method MOESP_auto
 dios-linear test  --config config.json --method MOESP_auto
 
-dios-linear train --config config.json --method ORT
+#dios-linear train --config config.json --method ORT
 dios-linear test  --config config.json --method ORT
-dios-linear train --config config.json --method ORT_auto
+#dios-linear train --config config.json --method ORT_auto
 dios-linear test  --config config.json --method ORT_auto
 
 
